4f062c0c8955ab5b2cf79caa6b07003eb27f27f1
Desiree Lerma <desi.lerma96@yahoo.com>
2020-10-27 15:35:25 -0500

    commit tree 0d744f2e193bbb11ee928166c2e6ae7bb1d8279f

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,6 +14,14 @@ require 'pry'
   #   }
   # }
   
-def remove_strawberry(contacts)
-
-end
+def remove_strawberry(contacts) # begin method
+  contacts.each do |person, contact_details_hash| #iterate over 1st level, each contact and their hashes
+    if person == "Freddy Mercury" # we want to delete from Freddy, not Jon
+      contact_details_hash.each do |attribute, data| # iterate over next level, go through the attr. and their data
+        if attribute == :favorite_ice_cream_flavors # we want to delete from fave ice cream
+          data.delete_if {|ice_cream| ice_cream == "strawberry"} # use delete_if to eliminate strawberry
+        end
+      end
+    end
+  end
+end # remember to close out each iteration
